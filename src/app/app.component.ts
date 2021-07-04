47dceeb12c22858df71bb2ecff9d21024a819df2
Lesley_Heyer <pingpongkat@yahoo.com>
2021-07-03 19:29:43 -0500

    working on search function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,24 @@ import { Satellite } from './satellite'
 export class AppComponent {
   title = 'orbit-report2';
   sourceList: Satellite[];
-  // displayList: Satellite[];
+  displayList: Satellite[];
 
-  constructor() {
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+  
+    this.displayList = matchingSatellites;
+  }
+
+  constructor(displayList = []) {
     this.sourceList = [];
+    this.displayList = displayList;
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
  
     window.fetch(satellitesUrl).then(function(response) {
@@ -23,23 +37,11 @@ export class AppComponent {
             let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
             this.sourceList.push(satellite);
           }
- 
+
+          this.displayList = this.sourceList.slice(0);
        }.bind(this));
     }.bind(this));
  
  } 
-
-//  search(searchTerm: string): void {
-//   let matchingSatellites: Satellite[] = [];
-//   searchTerm = searchTerm.toLowerCase();
-//   for(let i=0; i < this.sourceList.length; i++) {
-//      let name = this.sourceList[i].name.toLowerCase();
-//      if (name.indexOf(searchTerm) >= 0) {
-//         matchingSatellites.push(this.sourceList[i]);
-//      }
-//   }
-
-//   this.displayList = matchingSatellites;
-// }
  
 }
